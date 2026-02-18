69817002153ef31a5897751b6b7b718178c5fa47
Shynn Lawrence <shynn.lawrence@gmail.com>
2026-02-17 19:19:54 -0800

    Add CLI arg + start wiring in the vsr

--- autogen/vsr/src/main.rs
+++ autogen/vsr/src/main.rs
@@ -1,9 +1,10 @@
-use std::path::Path;
+use std::path::PathBuf;
 
 use crate::{
     c_gen::write_vsr_state_files, construct_vsr::load_all_vsr_substructs,
     proto_gen::write_proto_file,
 };
+use clap::Parser;
 
 pub mod c_gen;
 pub mod construct_vsr;
@@ -11,16 +12,24 @@ pub mod proto_gen;
 pub mod schema;
 use anyhow::Result;
 
+#[derive(Parser, Debug)]
+#[command(about = "Generate VSR proto + C artifacts to an output directory.")]
+struct Args {
+    #[arg(value_name = "OUTPUT_DIR")]
+    output_dir: PathBuf,
+}
+
 fn main() -> Result<()> {
+    let args = Args::parse();
     println!("starting");
     let substructs = load_all_vsr_substructs()?;
 
-    let proto_out = Path::new(env!("CARGO_MANIFEST_DIR")).join("generated/vsr.proto");
+    let proto_out = args.output_dir.join("vsr.proto");
     write_proto_file(&substructs, &proto_out)?;
     println!("wrote {}", proto_out.display());
 
-    let state_h_out = Path::new(env!("CARGO_MANIFEST_DIR")).join("generated/vsr_state.h");
-    let state_c_out = Path::new(env!("CARGO_MANIFEST_DIR")).join("generated/vsr_state.c");
+    let state_h_out = args.output_dir.join("vsr_state.h");
+    let state_c_out = args.output_dir.join("vsr_state.c");
     write_vsr_state_files(&substructs, &state_h_out, &state_c_out)?;
     println!("wrote {}", state_h_out.display());
     println!("wrote {}", state_c_out.display());
--- justfile
+++ justfile
@@ -4,8 +4,12 @@
 autogen:
     @command -v protoc >/dev/null || (echo "Missing protoc (Protocol Buffers compiler)." && exit 1)
     @command -v protoc-gen-nanopb >/dev/null || (echo "Missing protoc-gen-nanopb. Install with: python3 -m pip install --user nanopb protobuf" && exit 1)
-    (cd autogen/vsr && cargo run)
+# Generate into generated/ (makes it easy to test)
+    (cd autogen/vsr && cargo run -- generated)
     (cd autogen/vsr && protoc -I generated --plugin=protoc-gen-nanopb=$(command -v protoc-gen-nanopb) --nanopb_out=generated generated/vsr.proto)
+# Generate into the actual vsr spot
+    (cd autogen/vsr && cargo run -- ../../mila-embedded/src/mcu/vsr)
+    (cd autogen/vsr && protoc -I ../../mila-embedded/src/mcu/vsr --plugin=protoc-gen-nanopb=$(command -v protoc-gen-nanopb) --nanopb_out=../../mila-embedded/src/mcu/vsr ../../mila-embedded/src/mcu/vsr/vsr.proto)
 
 ### Dashboard Stuff ###
 setup_dashboard:
