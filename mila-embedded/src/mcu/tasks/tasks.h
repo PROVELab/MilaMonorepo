da3bf69ffd37d64551ea3e080a422e839b477106
Shynn Lawrence <shynn.lawrence@gmail.com>
2026-02-28 15:53:29 -0800

    I'm in my happy place

--- mila-embedded/src/mcu/tasks/tasks.h
+++ mila-embedded/src/mcu/tasks/tasks.h
@@ -31,8 +31,4 @@ void start_send_motor_task();
 #define CONSOLE_TASK_PRIO 11 // Mostly low priority but
 void start_console_task();
 
-// Logging
-#define LOGGING_TASK_PRIO 11 // Similar priority to ~console
-void start_logging_task();
-
 #endif
